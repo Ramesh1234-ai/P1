9841e752f87619ace5c8b709ac23ecad21690623
Ramesh1234-ai <Sinharishit04@gmail.com>
2026-04-06 23:00:24 +0530

    Fixed .gitignore - removed unwanted tracked files

--- backend/api/route/user.route.js
+++
@@ -1,8 +0,0 @@
-import { login } from "../controller/user.controller";
-import { register } from "../controller/user.controller";
-import { VerifyJwt } from "../middleware/auth.middleware";
-import express from express
-const router =express.Router();
-router.post("/login",VerifyJwt,login)
-router.post("/register",VerifyJwt,register)
-export default router;
--- backend/api/server.js
+++
@@ -1,5 +0,0 @@
-import { app } from "./app";
-const port =process.env.port||3000;
-app.listen(port,()=>{
-    console.log(`Your Server is running on http:localhost://${port}`)
-})
